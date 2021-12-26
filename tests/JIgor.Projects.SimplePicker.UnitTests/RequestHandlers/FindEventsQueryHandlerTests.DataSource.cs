cb96e4cf5a8e9826df1b180904b9752d52b57d75
José Igor <j.igor.ifce@gmail.com>
2021-12-26 16:38:14 -0300

    Starting off request handlers unit testing

---
+++ tests/JIgor.Projects.SimplePicker.UnitTests/RequestHandlers/FindEventsQueryHandlerTests.DataSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JIgor.Projects.SimplePicker.UnitTests.RequestHandlers
+{
+    public partial class FindEventsQueryHandlerTests
+    {
+        public static class DataSource()
+        {
+
+        }
+    }
+}
---
+++ tests/JIgor.Projects.SimplePicker.UnitTests/RequestHandlers/FindEventsQueryHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using JIgor.Projects.SimplePicker.Api.Database.Contracts;
+using JIgor.Projects.SimplePicker.Api.RequestHandlers.Handlers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+using static JIgor.Projects.SimplePicker.UnitTests.RequestHandlers.FindEventsQueryHandlerTests.DataSource;
+
+namespace JIgor.Projects.SimplePicker.UnitTests.RequestHandlers
+{
+    [TestClass]
+    public partial class FindEventsQueryHandlerTests
+    {
+        [TestMethod]
+        public async Task FindEventsQueryHandlerShouldReturnExpectedResult()
+        {
+        }
+
+        private static FindEventsQueryHandler CreateEventsQueryHandler(
+            IEventRepository eventRepository = null,
+            IMapper mapper = null)
+        {
+            eventRepository ??= Substitute.For<IEventRepository>();
+            mapper ??= Substitute.For<IMapper>();
+
+            return new FindEventsQueryHandler(eventRepository, mapper);
+        }
+    }
+}
