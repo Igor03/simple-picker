a2fd1933320b78291724e7e79237e917911eaa08
José Igor <j.igor.ifce@gmail.com>
2021-12-31 16:56:59 -0300

    Initializing request handlers unit testing

--- tests/JIgor.Projects.SimplePicker.UnitTests/RequestHandlers/FindEventsQueryHandlerTests.cs
+++ tests/JIgor.Projects.SimplePicker.UnitTests/RequestHandlers/FindEventsQueryHandlerTests.cs
@@ -1,23 +1,94 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using AutoMapper;
+using FluentAssertions;
 using JIgor.Projects.SimplePicker.Api.Database.Contracts;
+using JIgor.Projects.SimplePicker.Api.Dtos;
+using JIgor.Projects.SimplePicker.Api.Entities;
 using JIgor.Projects.SimplePicker.Api.RequestHandlers.Handlers;
+using JIgor.Projects.SimplePicker.Api.RequestHandlers.Queries;
+using JIgor.Projects.SimplePicker.Api.RequestHandlers.RequestResponses;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 
-using static JIgor.Projects.SimplePicker.UnitTests.RequestHandlers.FindEventsQueryHandlerTests.DataSource;
+using static JIgor.Projects.SimplePicker.UnitTests.RequestHandlers.RequestHandlersTestsDataSource;
 
 namespace JIgor.Projects.SimplePicker.UnitTests.RequestHandlers
 {
     [TestClass]
-    public partial class FindEventsQueryHandlerTests
+    public class FindEventsQueryHandlerTests
     {
         [TestMethod]
         public async Task FindEventsQueryHandlerShouldReturnExpectedResult()
         {
+            // Arrange
+            var eventEntities = (List<Event>)GetFindEventsQueryHandlerShouldReturnExpectedResultData()[0];
+            var mappedEventEntities = (List<EventDto>)GetFindEventsQueryHandlerShouldReturnExpectedResultData()[1];
+
+            var eventRepository = Substitute.For<IEventRepository>();
+            var mapper = Substitute.For<IMapper>();
+
+            eventRepository.FindEventsAsync(CancellationToken.None)
+                .Returns(eventEntities);
+
+            mapper.Map<IEnumerable<EventDto>>(Arg.Any<List<Event>>())
+                .Returns(mappedEventEntities);
+
+            var queryHandler = FindEventsQueryHandler(eventRepository, mapper);
+
+            // Act
+            var result = await queryHandler.Handle(new FindEventsQuery(), CancellationToken.None)
+                .ConfigureAwait(false);
+
+            // Assert
+            _ = result.Value.Should().NotBeNull().And
+                .BeOfType<FindEventsQueryResponses.Success>()
+                .Which.Events.Should().BeEquivalentTo(mappedEventEntities);
         }
 
-        private static FindEventsQueryHandler CreateEventsQueryHandler(
+        [TestMethod]
+        public async Task FindEventsQueryHandlerShouldReturnNotFound()
+        {
+            // Arrange
+            const string outputMessage = "There aren't any active events at the moment!";
+
+            var eventRepository = Substitute.For<IEventRepository>();
+            eventRepository.FindEventsAsync(CancellationToken.None)
+                .Returns(new List<Event>());
+
+            var queryHandler = FindEventsQueryHandler(eventRepository);
+
+            // Act
+            var result = await queryHandler.Handle(new FindEventsQuery(), CancellationToken.None)
+                .ConfigureAwait(false);
+
+            // Assert
+            _ = result.Value.Should().NotBeNull()
+                .And.BeOfType<FindEventsQueryResponses.NotFound>()
+                .Which.Message.Should().Be(outputMessage);
+        }
+
+        [TestMethod]
+        public async Task FindEventsQueryHandlerShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var queryHandler = FindEventsQueryHandler();
+
+            // Act
+            Func<Task> result = async () => await
+                queryHandler.Handle(default!, CancellationToken.None)
+                .ConfigureAwait(false);
+
+            // Assert
+            _ = await result.Should()
+                .ThrowExactlyAsync<ArgumentNullException>(because: "Was provided a null value as a parameter.")
+                .ConfigureAwait(false);
+        }
+
+        private static FindEventsQueryHandler FindEventsQueryHandler(
             IEventRepository eventRepository = null,
             IMapper mapper = null)
         {
---
+++ tests/JIgor.Projects.SimplePicker.UnitTests/RequestHandlers/RequestHandlersTestsDataSource.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using JIgor.Projects.SimplePicker.Api.Dtos;
+using JIgor.Projects.SimplePicker.Api.Entities;
+
+namespace JIgor.Projects.SimplePicker.UnitTests.RequestHandlers
+{
+    public static class RequestHandlersTestsDataSource
+    {
+        public static object[] GetFindEventsQueryHandlerShouldReturnExpectedResultData()
+        {
+            var eventId1 = Guid.NewGuid();
+            var eventId2 = Guid.NewGuid();
+
+            var eventEntitySeed = new List<Event>()
+            {
+                new Event(
+                    eventId1,
+                    "Some title",
+                    "Some description",
+                    DateTime.Now,
+                    DateTime.Now.AddDays(1),
+                    false,
+                    new List<EventValue>()
+                    {
+                        new EventValue(Guid.NewGuid(), eventId1, "Breeze", false),
+                        new EventValue(Guid.NewGuid(), eventId1, "Brady", false),
+                    }),
+                new Event()
+                {
+                    Id = eventId2,
+                    Title = "Some title",
+                    Description = "Some description",
+                    StartDate = DateTime.Now,
+                    DueDate = DateTime.Now.AddDays(1),
+                    IsFinished = true,
+                    EventValues = new List<EventValue>()
+                    {
+                        new EventValue(Guid.NewGuid(), eventId2, "Montana", true),
+                    }
+                }
+            };
+
+            var mappedEntitySeed = new List<EventDto>()
+            {
+                new EventDto()
+                {
+                    Title = "Some title",
+                    Description = "Some description",
+                    StartDate = DateTime.Now,
+                    DueDate = DateTime.Now.AddDays(1),
+                    EventValues = new List<EventValueDto>()
+                    {
+                        new EventValueDto("Breeze"),
+                        new EventValueDto("Brady")
+                    }
+                },
+                new EventDto()
+                {
+                    Title = "Some title",
+                    Description = "Some description",
+                    StartDate = DateTime.Now,
+                    DueDate = DateTime.Now.AddDays(1),
+                    EventValues = new List<EventValueDto>()
+                    {
+                        new EventValueDto("Montana")
+                    }
+                }
+            };
+
+            return new object[] {eventEntitySeed, mappedEntitySeed};
+        }
+    }
+}
